49cb84a1b25d42cc0bc37382337d4e5bdc18e741
devshafi <frshafi49@gmail.com>
2023-06-27 00:35:15 +0600

    solved problem no 2621;sleep

---
+++ Easy/JavaScript/sleep.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, millis);
+    });
+}
+
+let t = Date.now();
+sleep(1000).then(() => console.log(Date.now())); // 100
